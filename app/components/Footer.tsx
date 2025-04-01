eba96feeedde00038f179493592df8574e20ba0a
Eric Welch <nimbalo99@gmail.com>
2025-04-01 22:13:32 +0100

    Add white ASI logo in footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,6 +2,7 @@
 import Link from 'next/link';
 import { Mail, Phone, MapPin, Facebook, X, Instagram, Linkedin, Youtube } from 'lucide-react';
 import React from 'react';
+import Image from "next/image";
 
 interface FooterProps {
     companyName?: string;
@@ -29,15 +30,22 @@ const Footer: React.FC<FooterProps> = ({
 
     return (
         <footer className="bg-gray-900 text-white relative z-50">
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
+            <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
                 {/* Top section with columns */}
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 mb-12">
                     {/* Company info */}
                     <div>
-                        <h3 className="text-xl font-bold mb-4">{companyName}</h3>
-                        <p className="text-gray-400 mb-4">
-                            {companyDescription}
-                        </p>
+                        <Image
+                            src="/asiLogoWhite.svg"
+                            alt="ASI Logo"
+                            width={120}
+                            height={120}
+                            priority
+                            className="object-contain h-[50px] md:h-[120px]"
+                        />
+                        {/*<p className="text-gray-400 mb-4">*/}
+                        {/*    {companyDescription}*/}
+                        {/*</p>*/}
                         {/*<div className="flex space-x-4">*/}
                         {/*    <a href="https://facebook.com" aria-label="Facebook" className="text-gray-400 hover:text-white transition-colors">*/}
                         {/*        <Facebook size={20} />*/}
@@ -115,13 +123,13 @@ const Footer: React.FC<FooterProps> = ({
                     <div>
                         <h3 className="text-xl font-bold mb-4">Contact Info</h3>
                         <ul className="space-y-4">
-                            <li className="flex items-start">
-                                <MapPin className="mr-2 h-5 w-5 text-gray-400 flex-shrink-0 mt-0.5" />
-                                <span className="text-gray-400">
-                  {address.street}<br />
-                                    {address.city}, {address.stateZip}
-                </span>
-                            </li>
+                {/*            <li className="flex items-start">*/}
+                {/*                <MapPin className="mr-2 h-5 w-5 text-gray-400 flex-shrink-0 mt-0.5" />*/}
+                {/*                <span className="text-gray-400">*/}
+                {/*  {address.street}<br />*/}
+                {/*                    {address.city}, {address.stateZip}*/}
+                {/*</span>*/}
+                {/*            </li>*/}
                             <li className="flex items-center">
                                 <Mail className="mr-2 h-5 w-5 text-gray-400" />
                                 <a href={`mailto:${email}`} className="text-gray-400 hover:text-white transition-colors">
