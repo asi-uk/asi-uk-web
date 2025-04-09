e174992aa0e586a3ee2229760ea84b179f8f0b3a
Eric Welch <nimbalo99@gmail.com>
2025-04-09 08:30:54 -0600

    Add contact email for project application forms

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -63,7 +63,14 @@ export default function Projects() {
                 <div className="text-left p-5">
 
                     <Heading1 text={"Project Funding"} />
-                    <p>The 2025 ASI UK project application cycle is now open. The project funding application deadline is the <span className={"font-bold"}>15th of May</span></p>
+                    <p>The 2025 ASI UK project application cycle is now open. Please send the completed application form and budget to <a
+                        href="mailto:evangelism@asiuk.org"
+                        className="text-blue-600 underline hover:text-blue-800"
+                    >
+                        evangelism@asiuk.org
+                    </a>.
+                        The project funding application deadline is the <span className={"font-bold"}>15th of May</span>
+                    </p>
 
                     <div className="bg-slate-50 rounded-2xl p-5 my-5 mb-10 flex flex-col items-center">
                         <span className={`text-xl text-asi-blue font-bold`}>Application Forms</span>
